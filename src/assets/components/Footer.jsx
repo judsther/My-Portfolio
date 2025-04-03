5063b852a851f308b9537601a7552658725d447d
judsther <j.arevaloguardado@gmail.com>
2025-04-03 15:57:44 -0600

    added icons

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -1,9 +1,12 @@
+
 export default function Footer() {
     return (
       <footer className="bg-zinc-100 text-indigo-300 text-center py-4 px-3">
-        <p className="text-neutral-400 text-[10pt] py-2 px-2 italic">
+        <p className="text-neutral-400 text-[12pt] py-2 px-2 italic">
         "May you experience the love of Christ, though it is so great you will never fully understand it. Then you will be filled with the fullness of life and power that comes from God." - Ephesians 3:19 NTV</p>
-        <p className="text-lg">&copy; 2025 Jud Developer - Todos los derechos reservados.</p>
+  
+
+        <p className="text-[10]">&copy; 2025 Jud Developer - Todos los derechos reservados.</p>
       </footer>
     );
   }
--- src/assets/components/Hero/Hero.jsx
+++ src/assets/components/Hero/Hero.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
+import { SiReact, SiJavascript, SiTailwindcss, SiBootstrap, SiLaravel, SiPhp, SiGit, SiFirebase } from "react-icons/si";
+
 
 export default function Hero() {
     return (
       <section className="h-auto bg-gradient-to-br from-yellow-200 to-pink-100 text-zinc-600 flex flex-col items-center justify-center text-center">
         {/* Imagen de perfil */}
-        <div className="pt-28">
+        <div className="md:pt-21 pt-25">
         <div className="w-52 h-52 rounded-full overflow-hidden border-4 border-white shadow-lg">
           <img
             src="https://res.cloudinary.com/djsdqleik/image/upload/v1741056587/pict250130_0258330000_ksx2wk.jpg" 
@@ -18,16 +20,32 @@ export default function Hero() {
         <div className="pb-14">
         <h1 className="text-4xl md:text-3xl lg:text-5xl font-extrabold mt-14">¡Hola, soy Judith Guardado!</h1>
         <p className="text-xl mt-2 font-semibold">Web Developer | Full Stack Jr.</p>
-        
+
+  {/* Iconos de habilidades */}
+        <div className="flex items-center justify-center py-3 px-2 md:px-0"> 
+<div className="flex gap-3 md:gap-10 text-3xl text-zinc-100 bg-zinc-600 rounded-full p-4 md:px-6">
+      <SiJavascript className="" />
+      <SiPhp className="" />
+      <SiReact className="" />
+      <SiTailwindcss className="" />
+      <SiBootstrap className="" />
+      <SiLaravel className="" />
+      <SiGit class="" />
+      <SiFirebase className="" />
+    </div>
+</div>
+
         {/* Bio */}
+        <div className="flex flex-col items-center justify-center px-8 ">
         <p className="mt-4 px-3 md:px-50  text-lg text-zinc-700">
-          Desarrolladora Full Stack Jr buscando aportar soluciones creativas y eficientes para la digitalización de empresas por medio de páginas/aplicaciones web completas. <br /> <br />Abierta a aportar con mi trabajo a causas que impacten positivamente en la sociedad.
+          Desarrolladora Full Stack Jr buscando aportar soluciones creativas y eficientes para la digitalización de empresas por medio de páginas/aplicaciones web completas. Abierta a aportar con mi trabajo a causas que impacten positivamente en la sociedad.
           Siempre en el camino de aprender y mejorar.
         </p>
         </div>
-        {/* Botón */}
+        </div>
 
-        <div className="pb-28">
+        {/* Botón */}
+        <div className="pb-18">
         <Link
           to="/proyectos" 
           className="mt-6 px-8 py-3 bg-indigo-400 text-white rounded-full text-lg font-semibold shadow-md hover:scale-105 transition"
@@ -35,6 +53,7 @@ export default function Hero() {
           Ver Proyectos
         </Link>
         </div>
+
       </section>
     );
   }

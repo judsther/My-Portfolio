62b87e832d4b16c37133430f3c68daae1657447c
judsther <j.arevaloguardado@gmail.com>
2025-05-20 14:40:55 -0600

    added techs to projects

--- src/assets/components/Projects/Projects.jsx
+++ src/assets/components/Projects/Projects.jsx
@@ -1,4 +1,5 @@
-import Navbar from "../Navbar/Navbar";
+import { SiBootstrap, SiCloudinary, SiEjs, SiFigma, SiFirebase, SiGit, SiJavascript, SiNextdotjs, SiReact, SiTailwindcss, SiVercel, SiVite } from "react-icons/si";
+
 
 export default function Projects() {
     const projects = [
@@ -6,24 +7,66 @@ export default function Projects() {
         title: "MeQue Estudio Creativo",
         link: "https://meque-studio.vercel.app",
         src: "",
+        tech: (
+          <div className="flex gap-3 md:gap-4 text-2xl text-zinc-600 rounded-full justify-center p-2">
+            <SiReact />
+            <SiNextdotjs />
+            <SiJavascript />
+            <SiTailwindcss />
+            <SiFigma />
+            <SiCloudinary />
+            <SiGit />
+            <SiVercel />
+          </div>
+        ),
         desc: "Mi proyecto Next.js más reciente, desde el diseño al desarrollo, optimizada para un SEO efectivo y tiempo de carga mejorado con CldImage para el uso de gran cantidad de imágenes dada la naturaleza del negocio.",
       },
       {
         title: "Website para negocio de productos/servicios en desarrollo.",
         link: "https://website-comercial.vercel.app",
         src: "",
+        tech: (
+          <div className="flex gap-3 md:gap-4 text-2xl text-zinc-600 rounded-full justify-center p-2">
+            <SiReact />
+            <SiNextdotjs />
+            <SiJavascript />
+            <SiTailwindcss />
+            <SiGit />
+            <SiVercel />
+          </div>
+        ),
         desc: "Web desarrollada con tecnología Next.js, su atractivo consiste en la transformación del navbar al hacer scroll gracias al uso de useState y useEffect de React en conjunto con otras librerías. Actualmente en proceso modo plantilla con placeholders lista para personalizar.",
       },
       {
         title: "Emmanuel Church",
         link: "https://emmanuel-church-six.vercel.app/",
         src: "https://res.cloudinary.com/djsdqleik/image/upload/v1743712319/Screenshot_2025-04-03_at_2.30.35_PM_gyat0q.png",
+        tech: (
+          <div className="flex gap-3 md:gap-4 text-2xl text-zinc-600 rounded-full justify-center p-2">
+            <SiReact />
+            <SiVite />
+            <SiJavascript />
+            <SiTailwindcss />
+            <SiCloudinary />
+            <SiGit />
+            <SiFirebase />
+          </div>
+        ),
         desc: "Página web desarrollada con tecnología Vite/React.js para una iglesia. Cuenta con diseño dinámico y moderno; y funcionalidad de 'posteo' de oraciones con Firebase.",
       },
       {
         title: "Edgeline Installations",
         link: "https://www.edgelineinstallations.com",
         src: "https://res.cloudinary.com/djsdqleik/image/upload/v1743712321/Screenshot_2025-04-03_at_2.29.30_PM_d0yor8.png",
+        tech: (
+          <div className="flex gap-3 md:gap-4 text-2xl text-zinc-600 rounded-full justify-center p-2">
+            <SiReact />
+            <SiVite />
+            <SiJavascript />
+            <SiBootstrap />
+            <SiGit />
+          </div>
+        ),
         desc: "Aplicación web básica para una empresa de carpintería especializada.",
       },
       // {
@@ -62,14 +105,17 @@ export default function Projects() {
                       <h3 className="text-2xl font-semibold text-indigo-400">
                         {project.title}
                       </h3>
-                      <p className="text-zinc-600">{project.desc}</p>
+                      <p className="text-zinc-600 md:mb-2">{project.desc}</p>
+                          
+                                     {project.tech}
+                                  
                     </div>
 
                     <a
                       href={project.link}
                       target="_blank"
                       rel="noopener noreferrer"
-                      className="relative flex text-indigo-400 font-bold z-20 group-hover:opacity-50 transition"
+                      className="relative flex text-indigo-400 font-bold z-20 group-hover:opacity-50 group-hover:text-white transition"
                     >
                       Ver Proyecto ➜
                     </a>
